72564eb4a8fc19c173e40c4ea1cd549da459f70c
Shashwat Pandey <39722327+Shaxpy@users.noreply.github.com>
2020-01-23 23:56:18 +0530

    Update Detect faces and smiles.py

--- Detect faces and smiles.py
+++ Detect faces and smiles.py
@@ -15,6 +15,7 @@ def detect(gray,frame):# get coordinates of rectangles for faces and detect eyes
             cv2.rectangle(roi_color, (ex,ey),(ex+ew,ey+eh),(0,255,0),2)
             
         smiles = smile_cascade.detectMultiScale(roi_gray, 1.6, 24) 
+        
         for(sx,sy,sw,sh) in smiles:           
             cv2.rectangle(roi_color, (sx,sy),(sx+sw,sy+sh),(0,0,255),2)
     return frame
