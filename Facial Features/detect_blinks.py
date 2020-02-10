df37bf6a8d70a2b76146f1ebc2bbd37ff665cc74
Shashwat Pandey <39722327+Shaxpy@users.noreply.github.com>
2020-02-11 01:06:22 +0530

    Update detect_blinks.py

--- Facial Features/detect_blinks.py
+++ Facial Features/detect_blinks.py
@@ -135,10 +135,9 @@ while True:
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
  
-	# if the `q` key was pressed, break from the loop
+	
 	if key == ord("q"):
 		break
 
-# do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
