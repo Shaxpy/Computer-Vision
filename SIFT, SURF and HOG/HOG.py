4443a8fc3ad6aa6fbafd404e515db26553a967f5
Shashwat Pandey <39722327+Shaxpy@users.noreply.github.com>
2020-02-27 14:43:35 +0530

    Create HOG.py

---
+++ SIFT, SURF and HOG/HOG.py
@@ -0,0 +1,28 @@
+from skimage.io import imread, imshow
+from skimage.transform import resize
+from skimage.feature import hog
+from skimage import exposure
+import matplotlib.pyplot as plt
+
+
+#reading the image
+img = imread('puppy.jpeg')
+resized_img = resize(img, (128,64)) 
+print(resized_img.shape)
+fd, hog_image = hog(resized_img, orientations=9, pixels_per_cell=(8, 8), 
+                    cells_per_block=(2, 2), visualize=True, multichannel=True)
+
+#If we divide the image into 8×8 cells and generate the histograms, 
+# we will get a 9 x 1 matrix for each cell.
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 8), sharex=True, sharey=True) 
+
+ax1.imshow(resized_img, cmap=plt.cm.gray) 
+ax1.set_title('Input image') 
+
+# Rescale histogram for better display 
+hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 10)) 
+
+ax2.imshow(hog_image_rescaled, cmap=plt.cm.gray) 
+ax2.set_title('Histogram of Oriented Gradients')
+
+plt.show()
